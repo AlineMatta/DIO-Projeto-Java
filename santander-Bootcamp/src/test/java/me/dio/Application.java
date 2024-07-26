d17c4265096f3942d5a2e592fdd8aeb737043ade
Aline Matta <line19@hotmail.com>
2024-07-25 21:03:41 -0300

    Santander Bootcamp

---
+++ santander-Bootcamp/src/test/java/me/dio/Application.java
@@ -0,0 +1,12 @@
+package me.dio;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootApplication
+class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
